195985b24bd895d465463cca8558cc7090e1e672
igel2000 <igel2000bk@yandex.ru>
2024-10-31 14:41:12 +0400

    requirements.txt

---
+++ 2.Python/test_import.py
@@ -0,0 +1,49 @@
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import plotly
+import psycopg2 #psycopg2-binary
+import mysql.connector #mysql-connector-python
+import sqlalchemy
+import pyspark
+import functional #pyfunctional
+import multiprocess #multiprocessing ???
+import asyncio
+import pytest
+import robot # robotframework
+import testify
+import requests
+import selenium
+import bs4
+import scrapy
+import sklearn #scikit-learn
+
+
+def show_version(module):
+    print(module.__name__, module.__version__)
+show_version(numpy)
+show_version(pandas)
+show_version(matplotlib)
+show_version(seaborn)
+show_version(plotly)
+show_version(psycopg2)
+show_version(mysql.connector)
+show_version(sqlalchemy)
+show_version(pyspark)
+show_version(functional)
+show_version(multiprocess)
+#show_version(asyncio)
+show_version(pytest)
+show_version(robot)
+show_version(testify)
+show_version(requests)
+show_version(selenium)
+show_version(bs4)
+show_version(scrapy)
+show_version(sklearn)
+
+#from pprint import pprint
+##pprint(multiprocessing.__dict__)
+#pprint(numpy.version.version)
+#pprint(multiprocessing.version.version)
