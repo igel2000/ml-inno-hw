0f8fcb19b689b8b0fa9eeae9134bd19d2dbb5980
igel2000 <igel2000bk@yandex.ru>
2024-12-08 11:04:28 +0400

    Чуть-чуть красоты в оформлении

--- 2.Python/2.06.1 Functional Programming/func.py
+++ 2.Python/2.06.1 Functional Programming/func.py
@@ -1,24 +1,48 @@
+"""
+Задание по функциональному программированию.
+
+igel2000@gmail.com
+"""
+__author__ = 'igel2000'
+
 from typing import List, Iterable, Dict
 import itertools
 import functools
 
 # region Задача 1 Фильтрация данных
 def three_or_more(s: str) -> bool:
-    """ Проверить, что переданная строка длинной 3 и более символа """
+    """Проверить, что переданная строка длинной 3 и более символа
+    Return
+    -------
+    * True, если строка длинной 3 и более символов 
+    * False, если None или строку менее 3 символов.
+
+    Parameters
+    ----------
+    s: str - проверяемая строка
+    
+    Raises
+    ------
+    TypeError - если передать не строку
+    """
     if s is None:
         return False
-    return len(s) >= 3
+    if not isinstance(s, str):
+        raise TypeError('Параметр s должен быть строкой')
+    result = True if len(s) >= 3 else False
+    return result
 
 def filter_more_than_three(l: Iterable) -> List:
-    """ Вернуть строки длинной более трех символов """
-    if l is None:
-        return []
-    if not isinstance(l, Iterable):
+    """Выбрать из l строки длинной более трех символов и вернуть их в виде списка
+    
+    Если передать None или неитерируемый объект - вернуть пустой список
+    """
+    if l is None or not isinstance(l, Iterable):
         return []
     return [i for i in l if three_or_more(i)] # альтернатива - list(filter(three_or_more, l))
 
 def println(l: Iterable) -> None:
-    """ Вывести список на экран """
+    """ Вывести список на экран - каждый элемент в отдельной строке"""
     for i in l:
         print(i)
 
@@ -26,7 +50,7 @@ def println(l: Iterable) -> None:
 
 #region Задача 2 Вложенные функции
 def is_int(s: str) -> bool:
-    """ Проверить, что строка содержит целое число """
+    """Проверить, что строка содержит целое число"""
     try:
         int(s)
         return True
@@ -34,17 +58,13 @@ def is_int(s: str) -> bool:
         return False
     
 def get_list(int_as_str: str) -> List:
-    """ Преобразовать строку с числами в список  """
-    if int_as_str is None:
-        return []
-    if len(int_as_str) == 0:
-        return []
-    if not isinstance(int_as_str, str):
+    """Преобразовать строку с числами в список чисел"""
+    if int_as_str is None or not isinstance(int_as_str, str) or len(int_as_str) == 0:
         return []
     return list(map(int, filter(lambda i: is_int(i), int_as_str.split(" "))))
 
 def sort_func(f, *args, **kwargs) -> List:
-    """ Отсортировать результать функции f """
+    """Отсортировать результать функции f, передав в неё все остальные параметры"""
     result_f = f(*args, **kwargs)
     sorted_result = sorted(result_f)
     return sorted_result
@@ -53,7 +73,7 @@ def sort_func(f, *args, **kwargs) -> List:
 
 #region Задача 3 Перестановки строк
 def get_permutations(s: str, n: int) -> List:
-    """ Получить все перестановки длинной n из строки s в лексикографическом порядке """
+    """Получить все перестановки длинной n из строки s в лексикографическом порядке"""
     s1 = sorted(s) # отсортировать, чтобы перестановки были в лексикографическом порядке
     r = ["".join(p) for p in itertools.permutations(s1, n)]
     return r
@@ -61,14 +81,14 @@ def get_permutations(s: str, n: int) -> List:
 
 #region Задача 4 Комбинации символов
 def get_combinations(s: str, k: int) -> List:
-    """ Получить все комбинации длинной не более k из строки s """
+    """Получить все комбинации длинной не более k из строки s"""
     r = ["".join(p) for i in range(1, k+1) for p in itertools.combinations(s, i)]
     return r
 #endregion
 
 #region Задача 5 Функция с частичными аргументами
 def sort_users_by_age(users: List, ascending = True) -> Dict:
-    """ Сортировать список users по возрасту """
+    """Сортировать список users по возрасту"""
     if users is None or len(users)==0 or not isinstance(users, List):
         return []
     return sorted(users, key = lambda x: x['age'], reverse = not ascending)
